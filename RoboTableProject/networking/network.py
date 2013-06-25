12b419ee518c22770b23da318cff5c1c7a8e178d
Arnaud Chen-yen-su <arnaud.chenyensu@gmail.com>
2013-06-25 18:53:12 +0000

    New package added

---
+++ RoboTableProject/networking/network.py
@@ -0,0 +1,15 @@
+import requests
+
+
+class Network(object):
+    """docstring for Network"""
+
+    def initiate(self):
+        # Open a socket or something else...
+        pass
+
+    def send(self, msg, ip_addr):
+        pass
+
+    def get_irs_location(self):
+        pass
