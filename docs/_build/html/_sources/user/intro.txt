38a7b6a5d9124b7a116a43cc7ea1378a44e4665e
Arnaud Chen-yen-su <arnaud.chenyensu@gmail.com>
2013-07-15 23:10:25 +0000

    Update

---
+++ docs/_build/html/_sources/user/intro.txt
@@ -0,0 +1,15 @@
+Intro
+=====
+
+The software is composed of two parts :
+* Scripts
+* Interface/Server web
+
+The server web is needed if you won't to be able to play with other robot table.
+Otherwise you can just launch the game with the scripts.
+
+Here's the five(or more) scripts : (maybe we should write this in the developer guide)
+*
+*
+*
+
--- docs/conf.py
+++ docs/conf.py
@@ -16,7 +16,9 @@ import sys, os
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('..'))
+sys.path.append(os.path.abspath('_themes'))
+import RoboTableProject
 
 # -- General configuration -----------------------------------------------------
 
@@ -81,7 +83,8 @@ exclude_patterns = ['_build']
 #show_authors = False
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+# pygments_style = 'sphinx'
+pygments_style = 'flask_theme_support.FlaskyStyle'
 
 # A list of ignored prefixes for module index sorting.
 #modindex_common_prefix = []
@@ -94,7 +97,8 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+# html_theme = 'default'
+html_theme = 'kr'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -102,7 +106,7 @@ html_theme = 'default'
 #html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
-#html_theme_path = []
+html_theme_path = ['_themes']
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
